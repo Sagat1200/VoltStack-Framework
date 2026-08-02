d267817acec2c3910cd72a789abda84989593725
FRANCISCO J. MORALES <105411569+Sagat1200@users.noreply.github.com>
2026-08-02 03:59:26 -0600

    refactor(controller-exceptions): update base exception class from runtimeexception to exception

--- src/Quantum/Controllers/Exceptions/ControllerException.php
+++ src/Quantum/Controllers/Exceptions/ControllerException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Quantum\Controllers\Exceptions;
 
-abstract class ControllerException extends \RuntimeException
+abstract class ControllerException extends \Exception
 {
     public function errorCode(): string
     {
